c3275fe994dd8e81a7404c8179698988e19d0514
joppe <joppen97@live.se>
2019-03-05 13:49:50 +0100

    Split into components

--- src/components/headers/sections/Layout.js
+++ src/components/headers/sections/Layout.js
@@ -2,33 +2,41 @@ import React, {Component} from 'react';
 import './Layout.scss';
 import data from '../../../api/headers.json';
 
-class Layout extends Component {
-    constructor(props) {
-        super(props);
-    }
+function SubSections(props) {
+    return (
+        <div key={props.i} className="Subsection closed">
+            <label key={props.i} className="SubsectionLabel">{props.name}</label>
+        </div>
+    )
+}
 
-    render() {
-        return (
-            <div id="Visual">
-                <div id="VisualDiv2">
-                    {data.Sections.map((item, index) => {
-                        return (
-                            <div key={index}>
-                                <label key={item.id} className="BroadTitle">{item.name}</label>
-                                {item.Subsections.map((subitem, i) => {
-                                    return (
-                                        <div key={i} className="Subsection closed">
-                                            <label key={i} className="SubsectionLabel">{subitem.name}</label>
-                                        </div>
-                                    )
-                                })}
-                            </div>
-                        )
-                    })}
-                </div>
+function BlockSubSection(props) {
+    return (
+        <div>
+            <label id=""></label>
+        </div>
+    )
+}
+
+function Layout() {
+    return (
+        <div id="Visual">
+            <div id="VisualDiv2">
+                {data.Sections.map((item, index) => {
+                    return (
+                        <div key={index}>
+                            <label key={item.id} className="BroadTitle">{item.name}</label>
+                            {item.Subsections.map((subitem, i) => {
+                                return (
+                                    <SubSections name={subitem.name} i={i}/>
+                                )
+                            })}
+                        </div>
+                    )
+                })}
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Layout
