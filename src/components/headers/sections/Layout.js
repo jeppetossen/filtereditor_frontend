8fad2b960b5ab1c35af5a8a98e75110dd84e3d55
joppe <joppen97@live.se>
2019-03-26 22:08:15 +0100

    Updated spectrum

--- src/components/headers/sections/Layout.js
+++ src/components/headers/sections/Layout.js
@@ -1,7 +1,7 @@
 import React, {Component, Fragment, useState} from 'react';
 import './Layout.scss';
 import data from '../../../api/headers.json';
-import Spectrum from '../../spectrum/spectrum.js';
+// import Spectrum from '../../spectrum/spectrum.js';
 
 
 class VisualEditor extends Component {
@@ -38,57 +38,56 @@ class VisualEditor extends Component {
                                     className="VisualEditor_ComboBoxRight_Button VisualButton">Reset
                             </button>
                             <br key={this.props.uid + 15}/>
-                            {/*<input key={props.uid} className="VisualEditor_ComboBoxRight_Button VisualButton"
-                                   type="button" value="Sound"/>*/}
-                            <button key={this.props.uid + 16} className="VisualEditor_ComboBoxRight_Button VisualButton">Icon
+                            <input key={this.props.uid + 16} className="VisualEditor_ComboBoxRight_Button VisualButton"
+                                   type="button" value="Sound"/>
+                            <button key={this.props.uid + 17} className="VisualEditor_ComboBoxRight_Button VisualButton">Icon
                             </button>
-                            <button key={this.props.uid + 17} className="VisualEditor_ComboBoxRight_Button VisualButton">Beam
+                            <button key={this.props.uid + 18} className="VisualEditor_ComboBoxRight_Button VisualButton">Beam
                             </button>
                         </div>
-                        <div key={this.props.uid + 18} className="VisualEditor_ComboBoxLeft">
-                            <div key={this.props.uid + 19} className="VisualEditor_ComboBoxColorPickerDiv">
-                                <div key={this.props.uid + 20}
+                        <div key={this.props.uid + 19} className="VisualEditor_ComboBoxLeft">
+                            <div key={this.props.uid + 20} className="VisualEditor_ComboBoxColorPickerDiv">
+                                <div key={this.props.uid + 21}
                                      className="VisualEditor_ComboBoxEnabledColor selectedSocket"></div>
-                                <label key={this.props.uid + 21} className="VisualEditor_ComboBoxLeftLabel">TX</label>
-                                {/*<input key={props.uid} type="text" className="SpectrumColorPicker" style="display: none;" />*/}
-                                <Spectrum/>
-                                {/*<div class="sp-replacer sp-light">
-                                    <div class="sp-preview">
-                                        <div class="sp-preview-inner"></div>
+                                <label key={this.props.uid + 22} className="VisualEditor_ComboBoxLeftLabel">TX</label>
+                                <input key={this.props.uid + 23} type="text" className="SpectrumColorPicker" style={{display: 'none'}} />
+                                <div key={this.props.uid + 24} className="sp-replacer sp-light">
+                                    <div key={this.props.uid + 25} className="sp-preview">
+                                        <div key={this.props.uid + 26} className="sp-preview-inner"></div>
                                     </div>
-                                    <div class="sp-dd">▼</div>
-                                </div>*/}
+                                    <div key={this.props.uid + 27} className="sp-dd">▼</div>
+                                </div>
                             </div>
-                            <div key={this.props.uid + 22} className="VisualEditor_ComboBoxColorPickerDiv">
-                                <div key={this.props.uid + 23}
+                            <div key={this.props.uid + 28} className="VisualEditor_ComboBoxColorPickerDiv">
+                                <div key={this.props.uid + 29}
                                      className="VisualEditor_ComboBoxEnabledColor selectedSocket"></div>
-                                <label key={this.props.uid + 24} className="VisualEditor_ComboBoxLeftLabel">BD</label>
-                                {/*<input key={props.uid} className="SpectrumColorPicker" style="display: none;" />*/}
-                                {/*<div class="sp-replacer sp-light">
-                                    <div class="sp-preview">
-                                        <div class="sp-preview-inner"></div>
+                                <label key={this.props.uid + 30} className="VisualEditor_ComboBoxLeftLabel">BD</label>
+                                <input key={this.props.uid + 31} className="SpectrumColorPicker" style={{display: 'none'}} />
+                                <div key={this.props.uid + 32} className="sp-replacer sp-light">
+                                    <div key={this.props.uid + 33} className="sp-preview">
+                                        <div key={this.props.uid + 34} className="sp-preview-inner"></div>
                                     </div>
-                                    <div class="sp-dd">▼</div>
-                                </div>*/}
+                                    <div key={this.props.uid + 35} className="sp-dd">▼</div>
+                                </div>
                             </div>
-                            <div key={this.props.uid + 25} className="VisualEditor_ComboBoxColorPickerDiv">
-                                <div key={this.props.uid + 26}
+                            <div key={this.props.uid + 36} className="VisualEditor_ComboBoxColorPickerDiv">
+                                <div key={this.props.uid + 37}
                                      className="VisualEditor_ComboBoxEnabledColor selectedSocket"></div>
-                                <label key={this.props.uid + 27} className="VisualEditor_ComboBoxLeftLabel">BG</label>
-                                {/*<input key={props.uid} className="SpectrumColorPicker" style="display: none;" />*/}
-                                {/*<div class="sp-replacer sp-light">
-                                    <div class="sp-preview">
-                                        <div class="sp-preview-inner"></div>
+                                <label key={this.props.uid + 38} className="VisualEditor_ComboBoxLeftLabel">BG</label>
+                                <input key={this.props.uid + 39} className="SpectrumColorPicker" style={{display: 'none'}} />
+                                <div key={this.props.uid + 40} className="sp-replacer sp-light">
+                                    <div key={this.props.uid + 41} className="sp-preview">
+                                        <div key={this.props.uid + 42} className="sp-preview-inner"></div>
                                     </div>
-                                    <div class="sp-dd">▼</div>
-                                </div>*/}
+                                    <div key={this.props.uid + 43} className="sp-dd">▼</div>
+                                </div>
                             </div>
                         </div>
-                        <div key={this.props.uid + 28} className="VisualEditorShowCaseDiv">
-                            <div key={this.props.uid + 29} className="VisualEditor_InnerItem">
-                                <div key={this.props.uid + 30} className="InnerItemContainer">
-                                    <div key={this.props.uid + 31} className="ItemDiv">
-                                        <label key={this.props.uid + 32} className="ItemTextLabel">Item Name</label>
+                        <div key={this.props.uid + 44} className="VisualEditorShowCaseDiv">
+                            <div key={this.props.uid + 45} className="VisualEditor_InnerItem">
+                                <div key={this.props.uid + 46} className="InnerItemContainer">
+                                    <div key={this.props.uid + 47} className="ItemDiv">
+                                        <label key={this.props.uid + 48} className="ItemTextLabel">Item Name</label>
                                         {/*<div id="ItemSocketsDiv_{{ block.id }}" class="ItemSocketsDiv">
 
                                         </div>*/}
