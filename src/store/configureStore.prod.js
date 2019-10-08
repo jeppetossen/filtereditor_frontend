02e31810905f604fd0aa9645ff2941d690c8cb64
joppe <joppen97@live.se>
2019-10-08 10:28:15 +0200

    fixed typo

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'
 //import thunk from 'redux-thunk'
 import api from '../middleware/api'
-import reducer from '../reducer'
+import reducer from '../reducers'
 
 const configureStore = preloadedState => createStore(
   reducer,
